099e2e761ab8a0029b1e374fedffc75843f1c04c
shrijanlakhey <lakheyshrijan@gmail.com>
2023-05-15 08:41:34 +0545

    added dummy about and login page in bookingapp

--- react/bookingapp/src/App.js
+++ react/bookingapp/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Home Page</h1>}/> {/* http://localhost:3000/  mah gayo bhane Home page bhannera dekhauxa*/} 
+        <Route path='/about' element={<h1>About Page</h1>}/> 
+        <Route path='/login' element={<h1>Login Page</h1>}/> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
