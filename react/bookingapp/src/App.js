f887f54bd8da9edb973b1ac1f86841c4d0ca189f
shrijanlakhey <lakheyshrijan@gmail.com>
2023-05-18 12:23:12 +0545

    added simple todo

--- react/bookingapp/src/App.js
+++ react/bookingapp/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Todo from './pages/todo';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<h1>Home Page</h1>}/> {/* http://localhost:3000/  mah gayo bhane Home page bhannera dekhauxa*/} 
+        <Route path='/' element={<Todo />}/> {/*linking todo.jsx file */}
         <Route path='/about' element={<h1>About Page</h1>}/> 
         <Route path='/login' element={<h1>Login Page</h1>}/> 
       </Routes>
---
+++ react/bookingapp/src/pages/todo.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./todo.css"
+
+const Todo = () => {
+    return(
+        <div id="todoContainer">
+            <div id="todoBox">
+                <h3 className='text-white text-center py-4'>My Todos</h3>
+
+                <div id="addTodoComponent" className='container'>
+                    <input type="text" name="addTodo" id="addTodo" placeholder='Add Todo' />
+                    <i class="fa-solid fa-plus"></i>
+                </div>
+
+                <div id="todos" className='container my-4'>
+                    <div className="alert completed my-3" role="alert">
+                        A simple primary alert—check it out!
+                    </div>
+                    
+                    <div className="alert pending my-3" role="alert">
+                        A simple primary alert—check it out!
+                    </div>
+                    
+                    <div className="alert pending my-3" role="alert">
+                        A simple primary alert—check it out!
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Todo
